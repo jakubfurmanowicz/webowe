3a7d07a40a8ddaccc7dedb383fb2b99f6ef5f0f0
jakubfurmanowicz <99475579+jakubfurmanowicz@users.noreply.github.com>
2022-12-22 09:18:49 +0100

    toDoList

--- 016-toDo/src/app/app.component.ts
+++ 016-toDo/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Task } from './models/task';
 })
 export class AppComponent {
   title = '016-toDo';
+  newTask: string = "";
 
   tasks: Task[] =
     [
@@ -32,4 +33,28 @@ export class AppComponent {
         isDone: false
       },
     ];
+  addTask() {
+    this.tasks.push(
+      {
+        description: this.newTask,
+        isDone: false
+      }
+    );
+    this.newTask = "";
+  }
+
+  removeDone() {
+    this.tasks = this.tasks.filter(task => !task.isDone);
+  }
+
+  save() {
+    let json = JSON.stringify(this.tasks);
+    localStorage.setItem('my-to-do-list', json);
+  }
+
+  load() {
+    let json = localStorage.getItem('my-to-do-list');
+    if (json)
+      this.tasks = JSON.parse(json);
+  }
 }
